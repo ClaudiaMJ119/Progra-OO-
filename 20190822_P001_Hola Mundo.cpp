31d31d99c565cec4cf1097f852da9f8630b1f02e
ClaudiaMJ119 <60190368+ClaudiaMJ119@users.noreply.github.com>
2020-01-22 10:43:50 -0600

    Add files via upload

---
+++ 20190822_P001_Hola Mundo.cpp
@@ -0,0 +1,12 @@
+/* AUTOR: Martínez Jiménez Claudia
+Descripción: Primer programa Hola Mundo
+Fecha: 22 de agosto del 2019
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<< "Hello world!";
+}
